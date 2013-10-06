db13ad6576963cfd7b027f6f0edf2c17e298938b
JoeBew42 <joebew42@gmail.com>
2013-10-06 14:19:26 +0200

    Test: Adding a directory that is reached by a symbolic link

--- tests/check_cwatch.c
+++ tests/check_cwatch.c
@@ -158,6 +158,26 @@ START_TEST(adds_a_directory_to_the_watch_list)
 }
 END_TEST
 
+START_TEST(adds_a_directory_that_is_reached_by_symlink_to_the_watch_list)
+{
+    uint32_t event_mask = 0;
+
+    int fd = 1;
+    LIST *list_wd = list_init();
+
+    char *real_path = "/home/cwatch/";
+    char *symlink = "/home/symlink_to_cwatch";
+
+    add_to_watch_list(real_path, symlink, fd, list_wd);
+
+    LIST_NODE *link_node = get_link_node_from_path(symlink, list_wd);
+
+    ck_assert_ptr_ne(link_node, NULL);
+
+    list_free(list_wd);
+}
+END_TEST
+
 Suite *cwatch_suite(void)
 {
     Suite *s = suite_create("cwatch");
@@ -175,6 +195,7 @@ Suite *cwatch_suite(void)
     tcase_add_test(tc_core, creates_a_wd_data);
     tcase_add_test(tc_core, creates_a_link_data);
     tcase_add_test(tc_core, adds_a_directory_to_the_watch_list);
+    tcase_add_test(tc_core, adds_a_directory_that_is_reached_by_symlink_to_the_watch_list);
     tcase_add_test(tc_core, formats_command_correctly_using_special_characters);
 
     suite_add_tcase(s, tc_core);
