efe1b89e065b3a1ea9e159761908b79acb35c06e
NaN <enzodicicco@gmail.com>
2014-05-18 23:57:51 +0200

    Setting srunner_set_fork_status to CK_NOFORK
    Ask to `check' to not fork new processes for any test case.
    This can be very useful for the debugging of the tests.

--- tests/check_cwatch.c
+++ tests/check_cwatch.c
@@ -511,6 +511,7 @@ int main(void)
     int number_failed;
     Suite *s = cwatch_suite();
     SRunner *sr = srunner_create(s);
+    srunner_set_fork_status(sr, CK_NOFORK);
     srunner_run_all(sr, CK_NORMAL);
     number_failed = srunner_ntests_failed(sr);
     srunner_free (sr);
